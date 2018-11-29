f53683e4b75eb7344a0cf6eb65430787a856db26
Matt <dankfury9@gmail.com>
2018-11-29 13:44:17 +0800

    Update general.py

--- modules/general.py
+++ modules/general.py
@@ -621,6 +621,9 @@ class General:
                         if user is None:
                             user = "Unknown User"
                         e.description += f"<:trade:516533473813594133> Traded With : {user}"
+                if db.store.count({"card_name":x['card_name']}):
+                    priceon = [y['card_price'] for y in db.store.find({"card_name":card})][0]
+                    e.description += "\n<:gold:514791023671509003> Price on store : {}".format(priceon)
 
                 if x['gifted_by'] != "None":
                     e.description += ":gift: Gifted by : {}".format(discord.utils.get(self.bot.get_all_members(), id=x['gifted_by']))
