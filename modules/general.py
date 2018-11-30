897c094a7c76701b139e300fd1c465e5bf58eb00
Matt <dankfury9@gmail.com>
2018-11-30 14:19:23 +0800

    Update general.py

--- modules/general.py
+++ modules/general.py
@@ -582,6 +582,7 @@ class General:
             }
             if db.user_cards.count({"user_id":ctx.author.id, "card_name":card}):
                 db.user_cards.update_one({"user_id":ctx.author.id}, {'$inc':{'amount':1}})
+                await ctx.send('updated database value')
             else:
                 db.user_cards.insert_one(data)
 
